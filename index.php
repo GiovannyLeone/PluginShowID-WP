f98357b7a3b4d0e02dbe89a99d3ca3fb84047cfd
Giovanny Leone <97744369+GiovannyLeone@users.noreply.github.com>
2022-08-26 01:00:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,10 @@ class IdUser{
     @$explodeURLRequest = explode('?', $explodeURL[3]); # 2° time, explode/Split the "?" Request in URL
     @$serverAdm .= $explodeURL[1] . "/" . "wp-admin/" . $explodeURLRequest[0]; #Create URL for conditional, That "$explodeURL[0]" make the URL, always stay with the file name of the page
     @$serverUser = "http:/" . $_SERVER['SCRIPT_NAME']; #Take the URL of current page
+    @$fixServerUser = "http://" . $explodeURL[1] . "/" . "wp-admin/index.php";
+
         # Conditional for this plugin don´t work in dashboard
-        if ($serverAdm != $serverUser) {
+        if ($serverAdm != $serverUser && $fixServerUser != $serverUser) {
             # Verifying if exist a session
             if ( ! function_exists( 'tutsup_session_start' ) ) {
                 # Function to Session
